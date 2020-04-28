41eab9729b10878c29a0dcd7d5d6fe68549d4122
jasminluka <jasminluka007@gmail.com>
2020-04-29 00:03:38 +0200

    Changed proxy and apollo uri

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import { setContext } from 'apollo-link-context';
 import App from './App';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://merng-apollo-server.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
